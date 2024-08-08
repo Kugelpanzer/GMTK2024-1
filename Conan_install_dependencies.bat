b92c8e88d04b4a38d2df502430667a613c478ef2
Jovan Batnožić <Jovan.Batnozic@tomtom.com>
2024-08-08 19:23:55 +0200

    Fix build files

--- Conan_install_dependencies.bat
+++ Conan_install_dependencies.bat
@@ -9,19 +9,19 @@ SET BUILD_ARGS=--build=missing --build=outdated --build=cascade
 SET EXTRA_ARGS=--settings:host libzt*:compiler.cppstd=17 ^
                --settings:build libzt*:compiler.cppstd=17
 
-conan install . -of _Build/%PROJECT_NAME%-x64-Debug/ ^
+conan install . -of _Build/%PROJECT_NAME%-x64/ ^
     %PROFL_ARGS% ^
     %BUILD_ARGS% ^
     %EXTRA_ARGS% ^
     -s:h build_type=Debug ^
     -s:b build_type=Debug
     
-:: conan install . -of _Build/%PROJECT_NAME%-x64-Release/ ^
-::     %PROFL_ARGS% ^
-::     %BUILD_ARGS% ^
-::     %EXTRA_ARGS% ^
-::     -s:h build_type=Release ^
-::     -s:b build_type=Release
+conan install . -of _Build/%PROJECT_NAME%-x64/ ^
+    %PROFL_ARGS% ^
+    %BUILD_ARGS% ^
+    %EXTRA_ARGS% ^
+    -s:h build_type=Release ^
+    -s:b build_type=Release
 
 REM To install RelWithDebInfo profile edit Conan_install_dependencies.bat.
 
@@ -30,3 +30,5 @@ REM To install RelWithDebInfo profile edit Conan_install_dependencies.bat.
 ::    %BUILD_ARGS% ^
 ::	  %EXTRA_ARGS% ^
 ::    -s build_type=RelWithDebInfo
+
+PAUSE
