3d10200c9c5aff41e449b59922a2e359aab6f939
Đorđe Mladenović <djolemlad@gmail.com>
2024-08-16 23:50:45 +0200

    alvin included propa

--- TestingProject/Source/Collisions.hpp
+++ TestingProject/Source/Collisions.hpp
@@ -1,3 +1,4 @@
+#include "Engine.hpp"
 #include <Hobgoblin/Alvin.hpp>
 #include <Hobgoblin/ChipmunkPhysics.hpp>
 #include <Hobgoblin/Math.hpp>
@@ -16,31 +17,34 @@ enum EntityIds {
     EID_LOOT
 };
 
-class CharacterInterface : public alvin::EntityBase {
+class CharacterInterface : public hg::alvin::EntityBase {
     public:
-        using EntitySuperclass = alvin::EntityBase;
+        using EntitySuperclass = hg::alvin::EntityBase;
 
-        static constexpr alvin::EntityTypeId ENTITY_TYPE_ID = EID_CHARACTER;
+        static constexpr hg::alvin::EntityTypeId ENTITY_TYPE_ID = EID_CHARACTER;
 
         static constexpr cpBitmask ENTITY_DEFAULT_CATEGORY = CAT_CHARACTER;
         static constexpr cpBitmask ENTITY_DEFAULT_MASK     = CAT_CHARACTER | CAT_TERRAIN | CAT_LOOT;
 };
 
-class TerrainInterface : public alvin::EntityBase {
+class TerrainInterface : public hg::alvin::EntityBase {
 public:
-    using EntitySuperclass = alvin::EntityBase;
+    using EntitySuperclass = hg::alvin::EntityBase;
 
-    static constexpr alvin::EntityTypeId ENTITY_TYPE_ID = EID_TERRAIN;
+    static constexpr hg::alvin::EntityTypeId ENTITY_TYPE_ID = EID_TERRAIN;
 
     static constexpr cpBitmask ENTITY_DEFAULT_CATEGORY = CAT_TERRAIN;
     static constexpr cpBitmask ENTITY_DEFAULT_MASK     = CAT_CHARACTER;
 };
-class LootInterface : public alvin::EntityBase {
+class LootInterface : public hg::alvin::EntityBase {
 public:
-    using EntitySuperclass = alvin::EntityBase;
+    using EntitySuperclass = hg::alvin::EntityBase;
 
-    static constexpr alvin::EntityTypeId ENTITY_TYPE_ID = EID_LOOT;
+    static constexpr hg::alvin::EntityTypeId ENTITY_TYPE_ID = EID_LOOT;
 
     static constexpr cpBitmask ENTITY_DEFAULT_CATEGORY = CAT_LOOT;
     static constexpr cpBitmask ENTITY_DEFAULT_MASK     = CAT_CHARACTER;
 };
+
+
+void InitColliders(hg::alvin::MainCollisionDispatcher& aDispatcher, hg::NeverNull<cpSpace*> aSpace);
